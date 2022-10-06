0ea7dd0369e2874939b8b37116e3f08a88308834
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-10-06 16:43:43 -0700

    Add project files.

---
+++ WebApplicationMySQL/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using Microsoft.EntityFrameworkCore;
+global using WebApplicationMySQL.Models;
+global using WebApplicationMySQL.Data;
---
+++ WebApplicationMySQL/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationMySQL.Models
+{
+    [Index(nameof(Email), IsUnique = true)]
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name field is required.")]
+        [StringLength(maximumLength:100, MinimumLength = 2, ErrorMessage ="Name must be between 2 and 100 Characters.")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Email field is required.")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
+        [Required(ErrorMessage = "Phone field is required.")]
+        [StringLength(maximumLength: 15, MinimumLength = 10, ErrorMessage = "Phone must be between 10 and 15 Characters.")]
+        public string Phone { get; set; }
+
+
+        [StringLength(maximumLength: 255, ErrorMessage = "Address must less than 255 Characters.")]
+        public string ?  Address { get; set; }
+
+
+    }
+}
