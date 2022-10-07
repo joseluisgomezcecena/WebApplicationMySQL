ab938bc54fc99d2a570af9c4fc78607e8ef3d1d0
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2022-10-06 17:10:00 -0700

    added category model.

---
+++ WebApplicationMySQL/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplicationMySQL.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Category Name is required.")]
+        public string Name { get; set; }
+
+        [Required]
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
